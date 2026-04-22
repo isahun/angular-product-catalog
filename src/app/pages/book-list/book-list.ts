071e0f636132ffa1a16ca319337660f6dae394e6
Irene <isahun7@gmail.com>
2026-04-22 11:33:00 +0200

    feat: add ProductList page using ProductService and ProductCard

--- src/app/pages/book-list/book-list.ts
+++ src/app/pages/book-list/book-list.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { BookCard } from '../../components/book-card/book-card';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
-  templateUrl: './book-list.html',
-  styleUrl: './book-list.css',
+  imports: [BookCard,],
+  templateUrl: './book-list.html'
 })
-export class BookList {}
+export class BookList {
+  private service = inject(BookService);
+  books = this.service.getBooks();
+}
